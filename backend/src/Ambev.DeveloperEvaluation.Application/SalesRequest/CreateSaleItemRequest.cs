a4d5efefe7224073fb5b28f5b29e92e7058feaf4
acacioamanda2 <a.acacioamanda@gmail.com>
2025-04-06 15:53:12 -0300

    added classes

---
+++ backend/src/Ambev.DeveloperEvaluation.Application/SalesRequest/CreateSaleItemRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Application.SalesRequest
+{
+    public class CreateSaleItemRequest
+    {
+        public Guid ProductId { get; set; }
+        public string ProductDescription { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ backend/src/Ambev.DeveloperEvaluation.Application/SalesResponse/SaleResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.Application.SalesResponse
+{
+    public class SaleResponse
+    {
+        public Guid Id { get; set; }
+        public string SaleNumber { get; set; }
+        public DateTime SaleDate { get; set; }
+        public string Customer { get; set; }
+        public string Branch { get; set; }
+        public decimal TotalAmount { get; set; }
+        public bool IsCancelled { get; set; }
+        public List<SaleItemResponse> Items { get; set; }
+    }
+
+}
